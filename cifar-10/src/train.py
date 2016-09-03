35bf8c6e552699b7d8546a0aa6236b296a500ee0
Hao Nguyen <hao.nguyen@nodomain.local>
2016-09-03 12:31:20 -0700

    cifar10-wip

--- cifar-10/src/train.py
+++ cifar-10/src/train.py
@@ -1,8 +1,35 @@
 import tensorflow as tf
 import input_pipeline as ip
 import model as m
+from configuration import Configuration
 
 
 if __name__ == "__main__":
 
-    print("Hello")
+    file_names = ["data/data_batch_%d.bin" % i for i in range(1, 6)]
+
+    pipeline = ip.DataPipeline(file_names)
+    train_x, train_y = pipeline.get_batch_op()
+
+    config = Configuration(
+        input_size=3072,
+        output_size=10,
+        batch_size=100,
+        num_epoch=1000000
+    )
+
+    convo_model = m.ConvoModel(train_x, train_y, config)
+    convo_model.initialize()
+
+    train_op = convo_model.train_op()
+
+    with tf.Session() as session:
+
+        init = tf.initialize_all_variables()
+
+        tf.train.start_queue_runners(session)
+        session.run(init)
+
+        for i in range(100):
+
+            session.run(train_op)
