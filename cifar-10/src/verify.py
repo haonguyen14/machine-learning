ef9ff4d4c7373962a1e5fc0ac2ffc89a03408a81
haonguyen14 <hao.n@Hao-Nguyens-iMac.local>
2016-10-17 14:19:37 -0700

    cifar10-wip

--- cifar-10/src/verify.py
+++ cifar-10/src/verify.py
@@ -12,11 +12,10 @@ def get_image(flat_array, w, h):
 
 def normalize_image(image):
 
-    max_val = np.max
-    (
+    max_val = np.max([
         np.abs(np.min(image)),
         np.abs(np.max(image))
-    )
+    ])
 
     scale = 0.0 if max_val < 1e-6 else (127.0 / max_val)
     offset = 128.0
