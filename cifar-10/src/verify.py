2c7709ad57cbb6d4a3e5b5fd12ba2fe5d74757e0
Hao Nguyen <hao.nguyen1214@gmail.com>
2016-10-17 14:16:09 -0700

    cifar10-wip

--- cifar-10/src/verify.py
+++ cifar-10/src/verify.py
@@ -8,3 +8,17 @@ def get_image(flat_array, w, h):
     array = np.transpose(array, (1, 2, 0))
 
     return Image.fromarray(array.astype(np.uint8), "RGB")
+
+
+def normalize_image(image):
+
+    max_val = np.max
+    (
+        np.abs(np.min(image)),
+        np.abs(np.max(image))
+    )
+
+    scale = 0.0 if max_val < 1e-6 else (127.0 / max_val)
+    offset = 128.0
+
+    return image * scale + offset
